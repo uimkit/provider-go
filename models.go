21caf5d5403a6d35ed9d2c780d3a5e16847a975e
fioman <fioman@qq.com>
2022-06-25 01:35:00 +0800

    fix

--- models.go
+++ models.go
@@ -392,3 +392,24 @@ type VideoMessageBody struct {
 	MD5      string            `json:"md5,omitempty"`      // 文件内容MD5
 	Thumb    *ThumbMessageBody `json:"thumb,omitempty"`    // 缩略图
 }
+
+// 评论类型
+type CommentType int
+
+const (
+	CommentTypeUndefined CommentType = iota // 未定义评论
+	CommentTypeText                         // 文本评论
+)
+
+// 动态类型
+type MomentType int
+
+const (
+	MomentTypeUndefined MomentType = iota // 未定义动态
+	MomentTypeText                        // 文本动态
+	MomentTypeImage                       // 图文动态
+	MomentTypeVideo                       // 视频动态
+	MomentTypeLink                        // 链接动态
+	MomentTypeWebcast                     // 直播
+	MomentTypeFeed                        // 视频号
+)
